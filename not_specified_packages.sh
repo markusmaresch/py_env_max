7dcaa98160bd0350d9e9d836aa727b7b0c9ae837
Markus Maresch <markus.maresch@gmail.com>
2021-10-20 19:20:36 +0200

    improved script

--- not_specified_packages.sh
+++ not_specified_packages.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 #
-# need to look at: pip, setuptools, wheel ...
+# list installed packages, that are NOT in requirements
 #
+requirements="requirements_miniconda.txt"
+cc_tmp="conda_created.tmp"
+
+grep CONDA_CREATE $requirements | \
+  awk '{print $1}' | sed -e "s/^#//g" > $cc_tmp
+
 for p in $(pip list | awk '{print $1}'); do
-  grep -q -i "^${p}==" requirements_miniconda.txt
+  grep -q -i "^${p}==" $requirements
   if [ $? -ne 0 ]; then
-    echo $p
+    #echo "Possible: $p"
+    grep -q -i "^${p}$" $cc_tmp
+    if [ $? -ne 0 ]; then
+      echo "$p"
+    fi
   fi
 done
+
+rm -f $cc_tmp
