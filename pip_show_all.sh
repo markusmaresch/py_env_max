f83724b6db9f84da8b2b343f68590eac4bf8c8c4
Markus Maresch <markus.maresch@gmail.com>
2021-10-23 21:25:23 +0200

    add warning before running

--- pip_show_all.sh
+++ pip_show_all.sh
@@ -3,6 +3,10 @@
 # create pip_show_all.txt ... takes a while
 #
 pip_show_all="pip_show_all.txt"
+
+echo "Recreate/Update ${pip_show_all} ? (will take a while) (Y/CTRL-C)"
+read ans
+
 rm -f $pip_show_all
 for p in $(pip list | awk '{print $1}' | grep -v -e "Package" -e "^--------"); do
   echo "======== ${p} BELOW"
