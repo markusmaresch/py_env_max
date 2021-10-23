c5900ebc0369da7bd436629685d2ea8e0da4f1ca
Markus Maresch <markus.maresch@gmail.com>
2021-10-23 21:21:28 +0200

    added pip_show_all

---
+++ pip_show_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# create pip_show_all.txt ... takes a while
+#
+pip_show_all="pip_show_all.txt"
+rm -f $pip_show_all
+for p in $(pip list | awk '{print $1}' | grep -v -e "Package" -e "^--------"); do
+  echo "======== ${p} BELOW"
+  pip show $p
+  echo "======== ${p} ABOVE"
+done | tee -a $pip_show_all
+
