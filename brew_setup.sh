1cffa63225c57220f7825924fa090a9e710fe9e5
Markus Maresch <markus.maresch@gmail.com>
2021-11-18 16:14:46 +0100

    minor version up and fixes

--- brew_setup.sh
+++ brew_setup.sh
@@ -38,10 +38,14 @@ other_installs_jupyter() {
   jupyter labextension install @bokeh/jupyter_bokeh
 }
 
-brew install libomp   # for xgboost
+# for xgboost
+brew install libomp
 
 brew install graphviz # needed to get 'dot' working
 # brew install gprof2dot # not tried
+
+# for kafka, zookeeper
+brew install kafka
 
 # docker for kafka
 brew install docker ## was: brew cask install docker
