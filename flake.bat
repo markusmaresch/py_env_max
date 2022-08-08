080ac5193d46405f85537da6a85f90aca63aec0e
Markus Maresch <markus.maresch@gmail.com>
2022-08-08 17:41:12 +0200

    nicer reporting of flake8 checker

--- flake.bat
+++ flake.bat
@@ -1,1 +1,5 @@
-flake8 --ignore E127,E501,E722,W504 --exclude _vendor
+@rem
+@rem call flake8 with our options .. make sure to use the proper environment
+@rem
+@flake8 --ignore E127,E501,E722,W504 --exclude _vendor
+if %ErrorLevel% equ 0 (echo ok) else (echo Fail)
