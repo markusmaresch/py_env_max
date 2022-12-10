e7a6cc9341d3a0df7b60ebbce029c35401370844
Markus Maresch <markus.maresch@gmail.com>
2022-12-10 20:45:10 +0100

    updated mini/opencv

---
+++ py_env_mini_all.sh
@@ -0,0 +1,74 @@
+# Level 1
+pip install absl-py==1.3.0 box2d-py==2.3.8 cachetools==5.2.0 certifi==2022.12.7 charset-normalizer==2.1.1 cycler==0.11.0 flatbuffers==22.12.6 fonttools==4.38.0
+pip check
+pip install gast==0.4.0 idna==3.4 joblib==1.2.0 keras==2.11.0 kiwisolver==1.4.4 libclang==14.0.6 llvmlite==0.39.1 markupsafe==2.1.1
+pip check
+pip install mccabe==0.7.0 multidict==6.0.3 numpy==1.23.5 oauthlib==3.2.2 pillow==9.3.0 pip==22.3.1 protobuf==3.19.6 pyasn1==0.4.8
+pip check
+pip install pycodestyle==2.10.0 pyflakes==3.0.1 pyobjc-core==9.0 pyparsing==3.0.9 pyperf==2.5.0 pyqt5-sip==12.11.0 pytz==2022.6 setuptools==65.6.3
+pip check
+pip install six==1.16.0 tensorboard-data-server==0.6.1 tensorboard-plugin-wit==1.8.1 tensorflow-estimator==2.11.0 tensorflow-io-gcs-filesystem==0.28.0 termcolor==2.1.1 threadpoolctl==3.1.0 toml==0.10.2
+pip check
+pip install typing-extensions==4.4.0 urllib3==1.26.13 wrapt==1.14.1 zipp==3.11.0
+pip check
+# Level 1
+# Level 2
+pip install babel==2.11.0 contourpy==1.0.6 flake8==6.0.0 google-pasta==0.2.0 grpcio==1.51.1 h5py==3.7.0 importlib-metadata==5.1.0 keras-preprocessing==1.1.2
+pip check
+pip install numba==0.56.4 opt-einsum==3.3.0 packaging==22.0 patsy==0.5.3 pipdeptree==2.3.3 pyasn1-modules==0.2.8 pyobjc-framework-cocoa==9.0 pyobjc-framework-libdispatch==9.0
+pip check
+pip install python-dateutil==2.8.2 requests==2.28.1 rsa==4.9 scipy==1.9.3 werkzeug==2.2.2 wheel==0.38.4
+pip check
+# Level 2
+# Level 3
+pip install astunparse==1.6.3 google-auth==2.15.0 markdown==3.4.1 matplotlib==3.6.2 pandas==1.5.2 pyobjc-framework-accounts==9.0 pyobjc-framework-addressbook==9.0 pyobjc-framework-adservices==9.0
+pip check
+pip install pyobjc-framework-adsupport==9.0 pyobjc-framework-applescriptkit==9.0 pyobjc-framework-applescriptobjc==9.0 pyobjc-framework-apptrackingtransparency==9.0 pyobjc-framework-audiovideobridging==9.0 pyobjc-framework-authenticationservices==9.0 pyobjc-framework-automaticassessmentconfiguration==9.0 pyobjc-framework-automator==9.0
+pip check
+pip install pyobjc-framework-avrouting==9.0 pyobjc-framework-backgroundassets==9.0 pyobjc-framework-businesschat==9.0 pyobjc-framework-calendarstore==9.0 pyobjc-framework-callkit==9.0 pyobjc-framework-cfnetwork==9.0 pyobjc-framework-classkit==9.0 pyobjc-framework-collaboration==9.0
+pip check
+pip install pyobjc-framework-colorsync==9.0 pyobjc-framework-contacts==9.0 pyobjc-framework-coreaudio==9.0 pyobjc-framework-corebluetooth==9.0 pyobjc-framework-coredata==9.0 pyobjc-framework-corehaptics==9.0 pyobjc-framework-corelocation==9.0 pyobjc-framework-coremedia==9.0
+pip check
+pip install pyobjc-framework-coremediaio==9.0 pyobjc-framework-coremidi==9.0 pyobjc-framework-coreml==9.0 pyobjc-framework-coremotion==9.0 pyobjc-framework-corespotlight==9.0 pyobjc-framework-corewlan==9.0 pyobjc-framework-cryptotokenkit==9.0 pyobjc-framework-datadetection==9.0
+pip check
+pip install pyobjc-framework-devicecheck==9.0 pyobjc-framework-discrecording==9.0 pyobjc-framework-diskarbitration==9.0 pyobjc-framework-dvdplayback==9.0 pyobjc-framework-eventkit==9.0 pyobjc-framework-exceptionhandling==9.0 pyobjc-framework-executionpolicy==9.0 pyobjc-framework-extensionkit==9.0
+pip check
+pip install pyobjc-framework-externalaccessory==9.0 pyobjc-framework-fileprovider==9.0 pyobjc-framework-findersync==9.0 pyobjc-framework-fsevents==9.0 pyobjc-framework-gamecenter==9.0 pyobjc-framework-gamecontroller==9.0 pyobjc-framework-healthkit==9.0 pyobjc-framework-imagecapturecore==9.0
+pip check
+pip install pyobjc-framework-imserviceplugin==9.0 pyobjc-framework-inputmethodkit==9.0 pyobjc-framework-installerplugins==9.0 pyobjc-framework-intents==9.0 pyobjc-framework-iosurface==9.0 pyobjc-framework-ituneslibrary==9.0 pyobjc-framework-kernelmanagement==9.0 pyobjc-framework-latentsemanticmapping==9.0
+pip check
+pip install pyobjc-framework-mailkit==9.0 pyobjc-framework-mediaaccessibility==9.0 pyobjc-framework-mediatoolbox==9.0 pyobjc-framework-metal==9.0 pyobjc-framework-metrickit==9.0 pyobjc-framework-mlcompute==9.0 pyobjc-framework-multipeerconnectivity==9.0 pyobjc-framework-naturallanguage==9.0
+pip check
+pip install pyobjc-framework-netfs==9.0 pyobjc-framework-network==9.0 pyobjc-framework-networkextension==9.0 pyobjc-framework-notificationcenter==9.0 pyobjc-framework-opendirectory==9.0 pyobjc-framework-osakit==9.0 pyobjc-framework-passkit==9.0 pyobjc-framework-pencilkit==9.0
+pip check
+pip install pyobjc-framework-photos==9.0 pyobjc-framework-photosui==9.0 pyobjc-framework-preferencepanes==9.0 pyobjc-framework-pushkit==9.0 pyobjc-framework-quartz==9.0 pyobjc-framework-replaykit==9.0 pyobjc-framework-safariservices==9.0 pyobjc-framework-screensaver==9.0
+pip check
+pip install pyobjc-framework-screentime==9.0 pyobjc-framework-scriptingbridge==9.0 pyobjc-framework-security==9.0 pyobjc-framework-servicemanagement==9.0 pyobjc-framework-sharedwithyoucore==9.0 pyobjc-framework-shazamkit==9.0 pyobjc-framework-social==9.0 pyobjc-framework-soundanalysis==9.0
+pip check
+pip install pyobjc-framework-speech==9.0 pyobjc-framework-storekit==9.0 pyobjc-framework-systemconfiguration==9.0 pyobjc-framework-systemextensions==9.0 pyobjc-framework-threadnetwork==9.0 pyobjc-framework-uniformtypeidentifiers==9.0 pyobjc-framework-usernotifications==9.0 pyobjc-framework-videosubscriberaccount==9.0
+pip check
+pip install pyobjc-framework-virtualization==9.0 pyobjc-framework-webkit==9.0 pyperformance==1.0.6 pytesseract==0.3.10 requests-oauthlib==1.3.1 scikit-learn==1.2.0
+pip check
+# Level 3
+# Level 4
+pip install google-auth-oauthlib==0.4.6 pyobjc-framework-accessibility==9.0 pyobjc-framework-applicationservices==9.0 pyobjc-framework-avfoundation==9.0 pyobjc-framework-avkit==9.0 pyobjc-framework-cloudkit==9.0 pyobjc-framework-contactsui==9.0 pyobjc-framework-coreaudiokit==9.0
+pip check
+pip install pyobjc-framework-coreservices==9.0 pyobjc-framework-coretext==9.0 pyobjc-framework-discrecordingui==9.0 pyobjc-framework-fileproviderui==9.0 pyobjc-framework-gamekit==9.0 pyobjc-framework-instantmessage==9.0 pyobjc-framework-intentsui==9.0 pyobjc-framework-linkpresentation==9.0
+pip check
+pip install pyobjc-framework-localauthentication==9.0 pyobjc-framework-mapkit==9.0 pyobjc-framework-medialibrary==9.0 pyobjc-framework-metalfx==9.0 pyobjc-framework-metalkit==9.0 pyobjc-framework-metalperformanceshaders==9.0 pyobjc-framework-modelio==9.0 pyobjc-framework-oslog==9.0
+pip check
+pip install pyobjc-framework-quicklookthumbnailing==9.0 pyobjc-framework-safetykit==9.0 pyobjc-framework-scenekit==9.0 pyobjc-framework-screencapturekit==9.0 pyobjc-framework-securityfoundation==9.0 pyobjc-framework-securityinterface==9.0 pyobjc-framework-sharedwithyou==9.0 pyobjc-framework-spritekit==9.0
+pip check
+pip install pyobjc-framework-syncservices==9.0 pyobjc-framework-usernotificationsui==9.0 pyobjc-framework-videotoolbox==9.0 pyobjc-framework-vision==9.0 statsmodels==0.13.5
+pip check
+# Level 4
+# Level 5
+pip install pyobjc-framework-dictionaryservices==9.0 pyobjc-framework-gameplaykit==9.0 pyobjc-framework-launchservices==9.0 pyobjc-framework-localauthenticationembeddedui==9.0 pyobjc-framework-mediaplayer==9.0 pyobjc-framework-metalperformanceshadersgraph==9.0 pyobjc-framework-searchkit==9.0 pyod==1.0.6
+pip check
+pip install tensorboard==2.11.0
+pip check
+# Level 5
+# Level 6
+pip install pyobjc==9.0 tensorflow==2.11.0
+pip check
+# Level 6
---
+++ py_env_opencv_all.sh
@@ -0,0 +1,18 @@
+# Level 1
+pip install certifi==2022.12.7 colorama==0.4.6 cycler==0.11.0 fonttools==4.38.0 imutils==0.5.4 joblib==1.2.0 kiwisolver==1.4.4 llvmlite==0.39.1
+pip check
+pip install mccabe==0.7.0 numpy==1.23.5 pillow==9.3.0 pip==22.3.1 pycodestyle==2.10.0 pyflakes==3.0.1 pyparsing==3.0.9 pyqrcode==1.2.1
+pip check
+pip install python-barcode==0.14.0 pyzbar==0.1.9 qrcode==7.3.1 setuptools==65.6.3 six==1.16.0 threadpoolctl==3.1.0 wheel==0.38.4 wincertstore==0.2
+pip check
+# Level 1
+# Level 2
+pip install contourpy==1.0.6 flake8==6.0.0 numba==0.56.4 opencv-contrib-python==4.6.0.66 opencv-python==4.6.0.66 opencv-python-headless==4.6.0.66 packaging==22.0 python-dateutil==2.8.2
+pip check
+pip install scipy==1.9.3
+pip check
+# Level 2
+# Level 3
+pip install matplotlib==3.6.2 scikit-learn==1.2.0
+pip check
+# Level 3
