266770614588d8b0186be4f3c31d299b24951452
Markus Maresch <markus.maresch@gmail.com>
2022-12-19 12:33:07 +0100

    update minimal required conda version

--- py_env_max.py
+++ py_env_max.py
@@ -22,7 +22,7 @@ class PyEnvMax:
         self.environment_name = None
         self.environment_default = self.get_environment_default()
         self.python_version_default = '3.9'
-        self.conda_version_minimum = '4.10.3'
+        self.conda_version_minimum = '22.11.1'
         return
 
     def get_environment_default(self) -> str:
