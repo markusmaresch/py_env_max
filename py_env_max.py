d2bc37a471bf2d7e5c6a81e44a542478250850a8
Markus Maresch <markus.maresch@gmail.com>
2023-03-05 23:22:06 +0100

    intermediate commit - trying to resolve migration issues

--- py_env_max.py
+++ py_env_max.py
@@ -21,8 +21,8 @@ class PyEnvMax:
     def __init__(self):
         self.environment_name = None
         self.environment_default = self.get_environment_default()
-        self.python_version_default = '3.9'
-        self.conda_version_minimum = '22.11.1'
+        self.python_version_default = '3.9'  # '3.10.9'
+        self.conda_version_minimum = '4.12'  # '22.11.1'  # '23.1.0'
         return
 
     def get_environment_default(self) -> str:
