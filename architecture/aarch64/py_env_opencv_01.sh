d1fc385ab0c509499db35ec5ce1ce5c81b52fa80
Markus Maresch <markus.maresch@gmail.com>
2023-04-04 01:09:48 +0200

    add best known opencv env for aarch64

---
+++ architecture/aarch64/py_env_opencv_01.sh
@@ -0,0 +1,12 @@
+# Level 1
+pip install appdirs==1.4.4 certifi==2022.12.7 charset-normalizer==3.1.0 cycler==0.11.0 darkdetect==0.8.0 docutils==0.19 fonttools==4.39.3 idna==3.4
+pip check
+pip install installer==0.7.0 kiwisolver==1.4.4 markupsafe==2.1.2 numpy==1.24.2 pillow==9.5.0 pip==23.0.1 ply==3.11 pycodestyle==2.10.0
+pip check
+pip install pygments==2.14.0 pygubu==0.30 pyparsing==3.0.9 pyqt5-sip==12.11.1 pyqt6-sip==13.4.1 pytz==2023.3 pyzbar==0.1.9 screeninfo==0.8.1
+pip check
+pip install setuptools==65.6.3 six==1.16.0 tk==0.1.0 toml==0.10.2 tomli==2.0.1 urllib3==1.26.15 wheel==0.38.4 xlsxwriter==3.0.9
+pip check
+pip install zipp==3.15.0
+pip check
+# Level 1
---
+++ architecture/aarch64/py_env_opencv_04.sh
@@ -0,0 +1,4 @@
+# Level 4
+pip install kivy==2.1.0 pyqt-builder==1.14.1
+pip check
+# Level 4
