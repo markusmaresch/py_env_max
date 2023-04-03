bbd0841e4836da7f609ea600c08abe6e49c15458
Markus Maresch <markus.maresch@gmail.com>
2023-04-04 01:17:08 +0200

    update minimal requirement

--- py_env_max.py
+++ py_env_max.py
@@ -22,7 +22,7 @@ class PyEnvMax:
         self.environment_name = None
         self.environment_default = self.get_environment_default()
         self.python_version_default = '3.9'  # '3.10.9'
-        self.conda_version_minimum = '4.12'  # '22.11.1'  # '23.1.0'
+        self.conda_version_minimum = '23.1.0'
         return
 
     def get_environment_default(self) -> str:
