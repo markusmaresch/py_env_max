a58c2de92a2ccbb05efb83e91213f8640d35621c
Markus Maresch <markus.maresch@gmail.com>
2023-04-04 18:38:42 +0200

    checked py_env_opencv for windows again

--- architecture/windows/py_env_opencv_02.bat
+++ architecture/windows/py_env_opencv_02.bat
@@ -3,9 +3,9 @@ pip install arena-api==2.3.3 asttokens==2.2.1 click==8.1.3 comm==0.1.2 contourpy
 pip check
 pip install imageio==2.26.0 importlib-metadata==6.0.0 importlib-resources==5.12.0 jedi==0.18.2 jinja2==3.1.2 jsonschema==4.17.3 jupyter-core==5.1.5 laspy==2.4.1
 pip check
-pip install matplotlib-inline==0.1.6 numba==0.56.4 opencv-contrib-python==4.7.0.72 opencv-python==4.7.0.72 opencv-python-headless==4.7.0.72 packaging==23.0 plotly==5.13.1 plyfile==0.7.4
+pip install matplotlib-inline==0.1.6 numba==0.56.4 opencv-contrib-python==4.7.0.72 opencv-python==4.7.0.72 opencv-python-headless==4.7.0.72 plotly==5.13.1 plyfile==0.7.4 prompt-toolkit==3.0.38
 pip check
-pip install prompt-toolkit==3.0.38 pyarrow==10.0.1 pyproj==3.4.1 pyqt6==6.4.2 python-dateutil==2.8.2 pywavelets==1.4.1 qrcode==7.4.2 requests==2.28.2
+pip install pyarrow==10.0.1 pyproj==3.4.1 pyqt6==6.4.2 pytest==7.2.2 python-dateutil==2.8.2 pywavelets==1.4.1 qrcode==7.4.2 requests==2.28.2
 pip check
 pip install scipy==1.10.1 shapely==2.0.1 tifffile==2023.2.28 werkzeug==2.2.3
 pip check
