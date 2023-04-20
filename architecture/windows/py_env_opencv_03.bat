e1c1f35c38c43bdad58876342b3025e1838fe04a
Markus Maresch <markus.maresch@gmail.com>
2023-04-20 16:20:10 +0200

    cleanup and update of py_env_opencv under windows

--- architecture/windows/py_env_opencv_03.bat
+++ architecture/windows/py_env_opencv_03.bat
@@ -1,6 +1,6 @@
 rem Level 3
-pip install flask==2.2.3 jupyter-client==8.1.0 matplotlib==3.7.1 nbformat==5.7.0 pandas==2.0.0 rerun-sdk==0.4.0 scikit-image==0.19.3 scikit-learn==1.2.2
+pip install flask==2.2.3 jupyter-client==8.2.0 matplotlib==3.7.1 nbformat==5.7.0 pandas==2.0.0 qt6-tools==6.5.0.1.3 rerun-sdk==0.4.0 scikit-image==0.19.3
 pip check
-pip install stack-data==0.6.2 yarg==0.1.9
+pip install scikit-learn==1.2.2 stack-data==0.6.2 yarg==0.1.9
 pip check
 rem Level 3
