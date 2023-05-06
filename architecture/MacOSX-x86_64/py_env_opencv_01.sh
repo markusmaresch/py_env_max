5aac59bdd8cf63f2a8a9ca1e91951d82890a7fc8
Markus Maresch <markus.maresch@gmail.com>
2023-05-06 23:04:49 +0200

    updated py_env_opencv for MacOSX

--- architecture/MacOSX-x86_64/py_env_opencv_01.sh
+++ architecture/MacOSX-x86_64/py_env_opencv_01.sh
@@ -1,18 +1,18 @@
 # Level 1
-pip install addict==2.4.0 attrs==23.1.0 certifi==2022.12.7 charset-normalizer==3.1.0 click==8.1.3 colorama==0.4.6 configargparse==1.5.3 cycler==0.11.0
+pip install addict==2.4.0 attrs==23.1.0 blinker==1.6.2 certifi==2022.12.7 charset-normalizer==3.1.0 click==8.1.3 colorama==0.4.6 configargparse==1.5.3
 pip check
-pip install dash-core-components==2.0.0 dash-html-components==2.0.0 dash-table==5.0.0 docopt==0.6.2 fastjsonschema==2.16.3 fonttools==4.39.3 idna==3.4 imutils==0.5.4
+pip install cycler==0.11.0 dash-core-components==2.0.0 dash-html-components==2.0.0 dash-table==5.0.0 docopt==0.6.2 fastjsonschema==2.16.3 fonttools==4.39.3 idna==3.4
 pip check
-pip install itsdangerous==2.1.2 joblib==1.2.0 kiwisolver==1.4.4 lazy-loader==0.2 llvmlite==0.39.1 lmdb==1.4.1 markupsafe==2.1.2 mccabe==0.7.0
+pip install imutils==0.5.4 itsdangerous==2.1.2 joblib==1.2.0 kiwisolver==1.4.4 lazy-loader==0.2 llvmlite==0.40.0 lmdb==1.4.1 markupsafe==2.1.2
 pip check
-pip install networkx==3.1 numpy==1.23.5 packaging==23.1 pillow==9.5.0 pip==23.1 platformdirs==3.2.0 pycodestyle==2.10.0 pyflakes==3.0.1
+pip install mccabe==0.7.0 networkx==3.1 numpy==1.24.1 packaging==23.1 pillow==9.5.0 pip==23.1.2 platformdirs==3.5.0 pycodestyle==2.10.0
 pip check
-pip install pyparsing==3.0.9 pypng==0.20220715.0 pyqrcode==1.2.1 pyqt6-qt6==6.5.0 pyqt6-sip==13.5.0 pyrsistent==0.19.3 python-barcode==0.14.0 python-dotenv==1.0.0
+pip install pyflakes==3.0.1 pyparsing==3.0.9 pypng==0.20220715.0 pyqrcode==1.2.1 pyqt6-qt6==6.5.0 pyqt6-sip==13.5.1 pyrsistent==0.19.3 python-barcode==0.14.0
 pip check
-pip install pytz==2023.3 pyyaml==6.0 pyzbar==0.1.9 qt6-applications==6.5.0.2.3 setuptools==67.6.1 six==1.16.0 tenacity==8.2.2 threadpoolctl==3.1.0
+pip install python-dotenv==1.0.0 pytz==2023.3 pyyaml==6.0 pyzbar==0.1.9 qt6-applications==6.5.0.2.3 setuptools==67.7.2 six==1.16.0 tenacity==8.2.2
 pip check
-pip install tqdm==4.65.0 traitlets==5.9.0 typing-extensions==4.5.0 tzdata==2023.3 urllib3==1.26.15 wheel==0.40.0 wincertstore==0.2 xlsxwriter==3.1.0
+pip install threadpoolctl==3.1.0 tqdm==4.65.0 traitlets==5.9.0 typing-extensions==4.5.0 tzdata==2023.3 urllib3==2.0.1 wheel==0.40.0 wincertstore==0.2
 pip check
-pip install zipp==3.15.0
+pip install xlsxwriter==3.1.0 zipp==3.15.0
 pip check
 # Level 1
