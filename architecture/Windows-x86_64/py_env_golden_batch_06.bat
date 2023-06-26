52b8a11026b87bad8030cd63a44984d969c13369
Markus Maresch <markus.maresch@gmail.com>
2023-06-26 18:15:08 +0200

    added golden batch environment

---
+++ architecture/Windows-x86_64/py_env_golden_batch_06.bat
@@ -0,0 +1,4 @@
+rem Level 6
+pip install jupyter-server==2.6.0
+pip check
+rem Level 6
---
+++ architecture/Windows-x86_64/py_env_golden_batch_07.bat
@@ -0,0 +1,4 @@
+rem Level 7
+pip install jupyter-lsp==2.2.0 jupyterlab-server==2.23.0 notebook-shim==0.2.3
+pip check
+rem Level 7
