bb43023544b1f9b0a4a08507bcb8e1dbdd0b6ea7
Markus Maresch <markus.maresch@gmail.com>
2023-07-05 11:40:41 +0200

    update py_env_golden_batch for Windows

--- architecture/Windows-x86_64/py_env_golden_batch_01.bat
+++ architecture/Windows-x86_64/py_env_golden_batch_01.bat
@@ -3,28 +3,30 @@ pip install aioitertools==0.11.0 async-timeout==4.0.2 attrs==23.1.0 babel==2.12.
 pip check
 pip install boto==2.49.0 certifi==2023.5.7 charset-normalizer==3.1.0 colorama==0.4.6 debugpy==1.6.7
 pip check
-pip install decorator==5.1.1 defusedxml==0.7.1 exceptiongroup==1.1.1 executing==1.2.0 fastjsonschema==2.17.1
+pip install decorator==5.1.1 defusedxml==0.7.1 exceptiongroup==1.1.2 executing==1.2.0 fastjsonschema==2.17.1
 pip check
-pip install fqdn==1.5.1 frozenlist==1.3.3 fsspec==2023.6.0 idna==3.4 jmespath==1.0.1
+pip install fonttools==4.40.0 fqdn==1.5.1 frozenlist==1.3.3 fsspec==2023.6.0 idna==3.4
 pip check
-pip install json5==0.9.14 jsonpointer==2.4 jupyterlab-pygments==0.2.2 markupsafe==2.1.3 mistune==3.0.1
+pip install jmespath==1.0.1 joblib==1.3.1 json5==0.9.14 jsonpointer==2.4 jupyterlab-pygments==0.2.2
 pip check
-pip install multidict==6.0.4 nest-asyncio==1.5.6 numpy==1.25.0 overrides==7.3.1 packaging==23.1
+pip install markupsafe==2.1.3 mistune==3.0.1 multidict==6.0.4 nest-asyncio==1.5.6 numpy==1.25.0
 pip check
-pip install pandocfilters==1.5.0 parso==0.8.3 pickleshare==0.7.5 pip==23.1.2 platformdirs==3.8.0
+pip install overrides==7.3.1 packaging==23.1 pandocfilters==1.5.0 parso==0.8.3 pickleshare==0.7.5
 pip check
-pip install prometheus-client==0.17.0 psutil==5.9.5 pure-eval==0.2.2 pycparser==2.21 pygments==2.15.1
+pip install pillow==9.5.0 pip==23.1.2 platformdirs==3.8.0 prometheus-client==0.17.0 psutil==5.9.5
 pip check
-pip install pyrsistent==0.19.3 python-json-logger==2.0.7 pytz==2023.3 pywin32==306 pywinpty==2.0.10
+pip install pure-eval==0.2.2 pycparser==2.21 pygments==2.15.1 pyrsistent==0.19.3 python-json-logger==2.0.7
 pip check
-pip install pyyaml==6.0 pyzmq==25.1.0 rfc3986-validator==0.1.1 send2trash==1.8.2 setuptools==67.8.0
+pip install pytz==2023.3 pywin32==306 pywinpty==2.0.10 pyyaml==6.0 pyzmq==25.1.0
 pip check
-pip install six==1.16.0 sniffio==1.3.0 soupsieve==2.4.1 tomli==2.0.1 tornado==6.3.2
+pip install rfc3986-validator==0.1.1 send2trash==1.8.2 setuptools==68.0.0 six==1.16.0 sniffio==1.3.0
 pip check
-pip install traitlets==5.9.0 typing-extensions==4.6.3 tzdata==2023.3 uri-template==1.3.0 urllib3==1.26.16
+pip install soupsieve==2.4.1 threadpoolctl==3.1.0 tomli==2.0.1 tornado==6.3.2 traitlets==5.9.0
 pip check
-pip install wcwidth==0.2.6 webcolors==1.13 webencodings==0.5.1 websocket-client==1.6.1 wheel==0.38.4
+pip install typing-extensions==4.7.1 tzdata==2023.3 uri-template==1.3.0 urllib3==1.26.16 wcwidth==0.2.6
 pip check
-pip install wrapt==1.15.0 zipp==3.15.0
+pip install webcolors==1.13 webencodings==0.5.1 websocket-client==1.6.1 wheel==0.40.0 wrapt==1.15.0
+pip check
+pip install zipp==3.15.0
 pip check
 rem Level 1
--- architecture/Windows-x86_64/py_env_golden_batch_05.bat
+++ architecture/Windows-x86_64/py_env_golden_batch_05.bat
@@ -1,4 +1,4 @@
 rem Level 5
-pip install ipykernel==6.23.3 nbconvert==7.6.0 s3fs==2023.6.0
+pip install ipykernel==6.24.0 nbconvert==7.6.0 s3fs==2023.6.0
 pip check
 rem Level 5
