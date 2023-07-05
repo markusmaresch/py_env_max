590335dea572dfdb58c75842053f7982e98070bc
Markus Maresch <markus.maresch@gmail.com>
2023-07-05 12:04:52 +0200

    updated py_env_golden_batch again for Windows

--- architecture/Windows-x86_64/py_env_golden_batch_01.bat
+++ architecture/Windows-x86_64/py_env_golden_batch_01.bat
@@ -1,32 +1,32 @@
 rem Level 1
 pip install aioitertools==0.11.0 async-timeout==4.0.2 attrs==23.1.0 babel==2.12.1 backcall==0.2.0
 pip check
-pip install boto==2.49.0 certifi==2023.5.7 charset-normalizer==3.1.0 colorama==0.4.6 debugpy==1.6.7
+pip install boto==2.49.0 certifi==2023.5.7 charset-normalizer==3.1.0 colorama==0.4.6 cycler==0.11.0
 pip check
-pip install decorator==5.1.1 defusedxml==0.7.1 exceptiongroup==1.1.2 executing==1.2.0 fastjsonschema==2.17.1
+pip install debugpy==1.6.7 decorator==5.1.1 defusedxml==0.7.1 exceptiongroup==1.1.2 executing==1.2.0
 pip check
-pip install fonttools==4.40.0 fqdn==1.5.1 frozenlist==1.3.3 fsspec==2023.6.0 idna==3.4
+pip install fastjsonschema==2.17.1 fonttools==4.40.0 fqdn==1.5.1 frozenlist==1.3.3 fsspec==2023.6.0
 pip check
-pip install jmespath==1.0.1 joblib==1.3.1 json5==0.9.14 jsonpointer==2.4 jupyterlab-pygments==0.2.2
+pip install idna==3.4 jmespath==1.0.1 joblib==1.3.1 json5==0.9.14 jsonpointer==2.4
 pip check
-pip install markupsafe==2.1.3 mistune==3.0.1 multidict==6.0.4 nest-asyncio==1.5.6 numpy==1.25.0
+pip install jupyterlab-pygments==0.2.2 kiwisolver==1.4.4 markupsafe==2.1.3 mistune==3.0.1 multidict==6.0.4
 pip check
-pip install overrides==7.3.1 packaging==23.1 pandocfilters==1.5.0 parso==0.8.3 pickleshare==0.7.5
+pip install nest-asyncio==1.5.6 numpy==1.25.0 overrides==7.3.1 packaging==23.1 pandocfilters==1.5.0
 pip check
-pip install pillow==9.5.0 pip==23.1.2 platformdirs==3.8.0 prometheus-client==0.17.0 psutil==5.9.5
+pip install parso==0.8.3 pickleshare==0.7.5 pillow==9.5.0 pip==23.1.2 platformdirs==3.8.0
 pip check
-pip install pure-eval==0.2.2 pycparser==2.21 pygments==2.15.1 pyrsistent==0.19.3 python-json-logger==2.0.7
+pip install prometheus-client==0.17.0 psutil==5.9.5 pure-eval==0.2.2 pycparser==2.21 pygments==2.15.1
 pip check
-pip install pytz==2023.3 pywin32==306 pywinpty==2.0.10 pyyaml==6.0 pyzmq==25.1.0
+pip install pyparsing==3.1.0 pyrsistent==0.19.3 python-json-logger==2.0.7 pytz==2023.3 pywin32==306
 pip check
-pip install rfc3986-validator==0.1.1 send2trash==1.8.2 setuptools==68.0.0 six==1.16.0 sniffio==1.3.0
+pip install pywinpty==2.0.10 pyyaml==6.0 pyzmq==25.1.0 rfc3986-validator==0.1.1 send2trash==1.8.2
 pip check
-pip install soupsieve==2.4.1 threadpoolctl==3.1.0 tomli==2.0.1 tornado==6.3.2 traitlets==5.9.0
+pip install setuptools==68.0.0 six==1.16.0 sniffio==1.3.0 soupsieve==2.4.1 threadpoolctl==3.1.0
 pip check
-pip install typing-extensions==4.7.1 tzdata==2023.3 uri-template==1.3.0 urllib3==1.26.16 wcwidth==0.2.6
+pip install tomli==2.0.1 tornado==6.3.2 traitlets==5.9.0 typing-extensions==4.7.1 tzdata==2023.3
 pip check
-pip install webcolors==1.13 webencodings==0.5.1 websocket-client==1.6.1 wheel==0.40.0 wrapt==1.15.0
+pip install uri-template==1.3.0 urllib3==1.26.16 wcwidth==0.2.6 webcolors==1.13 webencodings==0.5.1
 pip check
-pip install zipp==3.15.0
+pip install websocket-client==1.6.1 wheel==0.40.0 wrapt==1.15.0 zipp==3.15.0
 pip check
 rem Level 1
--- architecture/Windows-x86_64/py_env_golden_batch_02.bat
+++ architecture/Windows-x86_64/py_env_golden_batch_02.bat
@@ -1,12 +1,12 @@
 rem Level 2
 pip install aiosignal==1.3.1 anyio==3.7.0 asttokens==2.2.1 async-lru==2.0.2 beautifulsoup4==4.12.2
 pip check
-pip install bleach==6.0.0 cffi==1.15.1 comm==0.1.3 fpdf2==2.7.4 importlib-metadata==6.7.0
+pip install bleach==6.0.0 cffi==1.15.1 comm==0.1.3 contourpy==1.1.0 fpdf2==2.7.4
 pip check
-pip install jedi==0.18.2 jinja2==3.1.2 jsonschema==4.17.3 jupyter-core==5.3.1 matplotlib-inline==0.1.6
+pip install importlib-metadata==6.7.0 jedi==0.18.2 jinja2==3.1.2 jsonschema==4.17.3 jupyter-core==5.3.1
 pip check
-pip install prompt-toolkit==3.0.39 python-dateutil==2.8.2 requests==2.31.0 rfc3339-validator==0.1.4 scipy==1.11.1
+pip install matplotlib-inline==0.1.6 prompt-toolkit==3.0.39 python-dateutil==2.8.2 requests==2.31.0 rfc3339-validator==0.1.4
 pip check
-pip install terminado==0.17.1 tinycss2==1.2.1 yarl==1.9.2
+pip install scipy==1.11.1 terminado==0.17.1 tinycss2==1.2.1 yarl==1.9.2
 pip check
 rem Level 2
