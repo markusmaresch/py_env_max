b9729bc015f41ae9802685cbcf595d76d4d260aa
Markus Maresch <markus.maresch@gmail.com>
2023-08-14 23:42:29 +0200

    updated py_env_opencv

--- architecture/MacOSX-x86_64/py_env_opencv_01.sh
+++ architecture/MacOSX-x86_64/py_env_opencv_01.sh
@@ -5,24 +5,24 @@ pip install charset-normalizer==3.2.0 click==8.1.6 colorama==0.4.6 configargpars
 pip check
 pip install dash-core-components==2.0.0 dash-html-components==2.0.0 dash-table==5.0.0 defusedxml==0.7.1 docopt==0.6.2
 pip check
-pip install fastjsonschema==2.18.0 fonttools==4.41.1 idna==3.4 imutils==0.5.4 itsdangerous==2.1.2
+pip install fastjsonschema==2.18.0 fonttools==4.42.0 idna==3.4 imutils==0.5.4 itsdangerous==2.1.2
 pip check
-pip install joblib==1.3.1 kiwisolver==1.4.4 lazy-loader==0.3 llvmlite==0.40.0 lmdb==1.4.1
+pip install joblib==1.3.2 kiwisolver==1.4.4 lazy-loader==0.3 llvmlite==0.40.0 lmdb==1.4.1
 pip check
-pip install markupsafe==2.1.3 mccabe==0.7.0 nest-asyncio==1.5.6 networkx==3.1 numpy==1.24.4
+pip install markupsafe==2.1.3 mccabe==0.7.0 nest-asyncio==1.5.7 networkx==3.1 numpy==1.24.4
 pip check
-pip install packaging==23.1 pillow==9.5.0 pip==23.2.1 platformdirs==3.9.1 pycodestyle==2.10.0
+pip install packaging==23.1 pillow==9.5.0 pip==23.2.1 platformdirs==3.10.0 pycodestyle==2.11.0
 pip check
-pip install pyflakes==3.0.1 pyparsing==3.0.9 pypng==0.20220715.0 pyqrcode==1.2.1 pyqt6-qt6==6.5.2
+pip install pyflakes==3.1.0 pyparsing==3.0.9 pypng==0.20220715.0 pyqrcode==1.2.1 pyqt6-qt6==6.5.2
 pip check
 pip install pyqt6-sip==13.5.2 pyrsistent==0.19.3 python-barcode==0.15.1 python-dotenv==1.0.0 pytz==2023.3
 pip check
-pip install pyyaml==6.0.1 pyzbar==0.1.9 qt6-applications==6.5.0.2.3 rpds-py==0.9.2 ruff==0.0.280
+pip install pyyaml==6.0.1 pyzbar==0.1.9 qt6-applications==6.5.0.2.3 rpds-py==0.9.2 ruff==0.0.284
 pip check
-pip install setuptools==68.0.0 shiboken6==6.5.2 six==1.16.0 tenacity==8.2.2 threadpoolctl==3.2.0
+pip install setuptools==68.0.0 shiboken6==6.5.2 six==1.16.0 tenacity==8.2.3 threadpoolctl==3.2.0
 pip check
-pip install tqdm==4.65.0 traitlets==5.9.0 typing-extensions==4.7.1 tzdata==2023.3 urllib3==2.0.4
+pip install tqdm==4.66.1 traitlets==5.9.0 typing-extensions==4.7.1 tzdata==2023.3 urllib3==2.0.4
 pip check
-pip install wheel==0.41.0 wincertstore==0.2 xlsxwriter==3.1.2 zipp==3.16.2
+pip install wheel==0.41.1 wincertstore==0.2 xlsxwriter==3.1.2 zipp==3.16.2
 pip check
 # Level 1
--- architecture/MacOSX-x86_64/py_env_opencv_all.sh
+++ architecture/MacOSX-x86_64/py_env_opencv_all.sh
@@ -5,49 +5,49 @@ pip install charset-normalizer==3.2.0 click==8.1.6 colorama==0.4.6 configargpars
 pip check
 pip install dash-core-components==2.0.0 dash-html-components==2.0.0 dash-table==5.0.0 defusedxml==0.7.1 docopt==0.6.2
 pip check
-pip install fastjsonschema==2.18.0 fonttools==4.41.1 idna==3.4 imutils==0.5.4 itsdangerous==2.1.2
+pip install fastjsonschema==2.18.0 fonttools==4.42.0 idna==3.4 imutils==0.5.4 itsdangerous==2.1.2
 pip check
-pip install joblib==1.3.1 kiwisolver==1.4.4 lazy-loader==0.3 llvmlite==0.40.0 lmdb==1.4.1
+pip install joblib==1.3.2 kiwisolver==1.4.4 lazy-loader==0.3 llvmlite==0.40.0 lmdb==1.4.1
 pip check
-pip install markupsafe==2.1.3 mccabe==0.7.0 nest-asyncio==1.5.6 networkx==3.1 numpy==1.24.4
+pip install markupsafe==2.1.3 mccabe==0.7.0 nest-asyncio==1.5.7 networkx==3.1 numpy==1.24.4
 pip check
-pip install packaging==23.1 pillow==9.5.0 pip==23.2.1 platformdirs==3.9.1 pycodestyle==2.10.0
+pip install packaging==23.1 pillow==9.5.0 pip==23.2.1 platformdirs==3.10.0 pycodestyle==2.11.0
 pip check
-pip install pyflakes==3.0.1 pyparsing==3.0.9 pypng==0.20220715.0 pyqrcode==1.2.1 pyqt6-qt6==6.5.2
+pip install pyflakes==3.1.0 pyparsing==3.0.9 pypng==0.20220715.0 pyqrcode==1.2.1 pyqt6-qt6==6.5.2
 pip check
 pip install pyqt6-sip==13.5.2 pyrsistent==0.19.3 python-barcode==0.15.1 python-dotenv==1.0.0 pytz==2023.3
 pip check
-pip install pyyaml==6.0.1 pyzbar==0.1.9 qt6-applications==6.5.0.2.3 rpds-py==0.9.2 ruff==0.0.280
+pip install pyyaml==6.0.1 pyzbar==0.1.9 qt6-applications==6.5.0.2.3 rpds-py==0.9.2 ruff==0.0.284
 pip check
-pip install setuptools==68.0.0 shiboken6==6.5.2 six==1.16.0 tenacity==8.2.2 threadpoolctl==3.2.0
+pip install setuptools==68.0.0 shiboken6==6.5.2 six==1.16.0 tenacity==8.2.3 threadpoolctl==3.2.0
 pip check
-pip install tqdm==4.65.0 traitlets==5.9.0 typing-extensions==4.7.1 tzdata==2023.3 urllib3==2.0.4
+pip install tqdm==4.66.1 traitlets==5.9.0 typing-extensions==4.7.1 tzdata==2023.3 urllib3==2.0.4
 pip check
-pip install wheel==0.41.0 wincertstore==0.2 xlsxwriter==3.1.2 zipp==3.16.2
+pip install wheel==0.41.1 wincertstore==0.2 xlsxwriter==3.1.2 zipp==3.16.2
 pip check
 # Level 1
 # Level 2
-pip install contourpy==1.1.0 flake8==6.0.0 fpdf2==2.7.4 h5py==3.9.0 imageio==2.31.1
+pip install contourpy==1.1.0 flake8==6.1.0 fpdf2==2.7.5 h5py==3.9.0 imageio==2.31.1
 pip check
-pip install importlib-metadata==6.8.0 importlib-resources==6.0.0 jinja2==3.1.2 jupyter-core==5.3.1 numba==0.57.1
+pip install importlib-metadata==6.8.0 importlib-resources==6.0.1 jinja2==3.1.2 jupyter-core==5.3.1 numba==0.57.1
 pip check
-pip install opencv-contrib-python==4.8.0.74 opencv-python==4.8.0.74 opencv-python-headless==4.8.0.74 plotly==5.15.0 plyfile==1.0.1
+pip install opencv-contrib-python==4.8.0.76 opencv-python==4.8.0.76 opencv-python-headless==4.8.0.76 plotly==5.16.0 plyfile==1.0.1
 pip check
 pip install pyqt6==6.5.2 pyquaternion==0.9.9 pyside6-essentials==6.5.2 python-dateutil==2.8.2 pywavelets==1.4.1
 pip check
-pip install qrcode==7.4.2 qt6-tools==6.5.0.1.3 reactivex==4.0.4 referencing==0.30.0 requests==2.31.0
+pip install qrcode==7.4.2 qt6-tools==6.5.0.1.3 reactivex==4.0.4 referencing==0.30.2 requests==2.31.0
 pip check
-pip install retrying==1.3.4 scipy==1.11.1 shapely==2.0.1 tifffile==2023.7.18 werkzeug==2.2.3
+pip install retrying==1.3.4 scipy==1.11.1 shapely==2.0.1 tifffile==2023.8.12 werkzeug==2.2.3
 pip check
 # Level 2
 # Level 3
-pip install flask==2.2.5 influxdb-client==1.36.1 jsonschema-specifications==2023.7.1 matplotlib==3.7.2 pandas==2.0.3
+pip install flask==2.2.5 influxdb-client==1.37.0 jsonschema-specifications==2023.7.1 matplotlib==3.7.2 pandas==2.0.3
 pip check
 pip install pyside6-addons==6.5.2 scikit-image==0.21.0 scikit-learn==1.3.0 yarg==0.1.9
 pip check
 # Level 3
 # Level 4
-pip install dash==2.11.1 jsonschema==4.18.4 pipreqs==0.4.13 pyside6==6.5.2
+pip install dash==2.12.0 jsonschema==4.19.0 pipreqs==0.4.13 pyside6==6.5.2
 pip check
 # Level 4
 # Level 5
