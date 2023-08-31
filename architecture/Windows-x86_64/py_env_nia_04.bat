66552627e6861984fff6807f2df94f9aefe18197
Markus Maresch <markus.maresch@gmail.com>
2023-08-31 15:01:55 +0200

    updated py_env_nia

--- architecture/Windows-x86_64/py_env_nia_04.bat
+++ architecture/Windows-x86_64/py_env_nia_04.bat
@@ -1,5 +1,5 @@
 rem Level 4
-pip install "aiobotocore>=2.5.3" "argon2-cffi>=23.1.0" "ipython>=8.14.0" "isoduration>=20.11.0" "jsonschema>=4.19.0"
+pip install "aiobotocore>=2.5.4" "argon2-cffi>=23.1.0" "ipython>=8.14.0" "isoduration>=20.11.0" "jsonschema>=4.19.0"
 pip check
 pip install "pipreqs>=0.4.13" "pyside6>=6.5.2"
 pip check
--- architecture/Windows-x86_64/py_env_nia_08.bat
+++ architecture/Windows-x86_64/py_env_nia_08.bat
@@ -1,4 +1,4 @@
 rem Level 8
-pip install "jupyter-server>=2.7.2"
+pip install "jupyter-server>=2.7.3"
 pip check
 rem Level 8
