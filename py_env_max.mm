40f7bfcf5978120082c1383867a0a927f2c8fde4
Markus Maresch <markus.maresch@gmail.com>
2023-10-05 21:29:37 +0200

    updated py_env_max.mm

--- py_env_max.mm
+++ py_env_max.mm
@@ -1,4 +1,4 @@
-<map version="freeplane 1.9.13">
+<map version="freeplane 1.11.1">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
 <node TEXT="py_env_max" FOLDED="false" ID="ID_1558508256" CREATED="1568633597132" MODIFIED="1649591680210"><hook NAME="MapStyle" zoom="0.75">
     <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false"/>
@@ -6,7 +6,7 @@
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
-<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
 <arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="DEFAULT" ENDARROW="NONE"/>
 <font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
@@ -24,7 +24,7 @@
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
 <font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
 </stylenode>
@@ -39,7 +39,7 @@
 <arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
 </stylenode>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
 <font SIZE="18"/>
 </stylenode>
@@ -66,16 +66,16 @@
 </stylenode>
 </map_styles>
 </hook>
-<node TEXT="Database" POSITION="right" ID="ID_512964669" CREATED="1649591232695" MODIFIED="1649619306307">
+<node TEXT="Database" POSITION="bottom_or_right" ID="ID_512964669" CREATED="1649591232695" MODIFIED="1649619306307">
 <node TEXT="Tables" ID="ID_1372701864" CREATED="1649620461125" MODIFIED="1649620469113">
 <node TEXT="Config" ID="ID_922357515" CREATED="1649620457164" MODIFIED="1649620460450"/>
 <node TEXT="Packages" ID="ID_2092545" CREATED="1649620473286" MODIFIED="1649620476233"/>
 </node>
 </node>
-<node TEXT="Concept" POSITION="right" ID="ID_267867978" CREATED="1649615136559" MODIFIED="1649615139203">
+<node TEXT="Concept" POSITION="bottom_or_right" ID="ID_267867978" CREATED="1649615136559" MODIFIED="1649615139203">
 <node TEXT="always ONE environment present or assumed" ID="ID_59735333" CREATED="1649615141059" MODIFIED="1649615159475"/>
 </node>
-<node TEXT="Options" POSITION="right" ID="ID_1154867364" CREATED="1651431717138" MODIFIED="1651431721560">
+<node TEXT="Options" POSITION="bottom_or_right" ID="ID_1154867364" CREATED="1651431717138" MODIFIED="1651431721560">
 <node TEXT="Flags" ID="ID_1399099359" CREATED="1651431566870" MODIFIED="1651431716194">
 <node TEXT="--force" ID="ID_1839888695" CREATED="1651431572067" MODIFIED="1651431822170"/>
 <node TEXT="--env ENV" ID="ID_1495297248" CREATED="1651431519496" MODIFIED="1651431816996">
@@ -85,26 +85,23 @@
 <node TEXT="Actions" ID="ID_1986681682" CREATED="1651431728208" MODIFIED="1651431732405">
 <node TEXT="--upd_all" ID="ID_1652064161" CREATED="1651432044720" MODIFIED="1657663290640">
 <icon BUILTIN="button_ok"/>
-<node TEXT="-latest-versions" ID="ID_382657485" CREATED="1649594065735" MODIFIED="1649619211682">
-<node TEXT="# get latest versions from Pypi, and store in records" ID="ID_1024694016" CREATED="1649619216953" MODIFIED="1649619237979"/>
 </node>
-<node TEXT="-all" ID="ID_1348387512" CREATED="1649594062102" MODIFIED="1651425249124">
-<node TEXT="# try carefully updating out of date, with minimal conflichts" ID="ID_496920381" CREATED="1649619245854" MODIFIED="1651425266315">
-<node TEXT="complicated" ID="ID_855543586" CREATED="1649619269152" MODIFIED="1649619272945"/>
-<node TEXT="can fail" ID="ID_1728926041" CREATED="1649619273304" MODIFIED="1649619275724"/>
-</node>
-</node>
-</node>
-<node TEXT="-scriipts-export" ID="ID_1978411996" CREATED="1651432050356" MODIFIED="1682368439722">
+<node TEXT="-scripts-export" FOLDED="true" ID="ID_1978411996" CREATED="1651432050356" MODIFIED="1696531495731">
 <icon BUILTIN="button_ok"/>
 <node TEXT="create batch/shell scripts, build_&lt;yyyymm&gt;_0N_&lt;a-f&gt;.ext" FOLDED="true" ID="ID_1996910313" CREATED="1649593001802" MODIFIED="1649593080734">
 <node TEXT="# M Levels" ID="ID_118788360" CREATED="1649593089553" MODIFIED="1649593094807"/>
 <node TEXT="# up to N packages per level" ID="ID_1973847670" CREATED="1649593096100" MODIFIED="1649593112144"/>
 </node>
-<node TEXT="[-max N]" ID="ID_902090156" CREATED="1649592984387" MODIFIED="1649593000611"/>
-<node TEXT="[-sh|-bat]" LOCALIZED_STYLE_REF="default" ID="ID_505539982" CREATED="1648840754952" MODIFIED="1649591504272" COLOR="#000000" BACKGROUND_COLOR="#ffffff" STYLE="fork">
+</node>
+<node TEXT="--packages_install" ID="ID_1017325688" CREATED="1651432469190" MODIFIED="1696531626932">
+<icon BUILTIN="button_ok"/>
+<node TEXT="P0 P1 .. Pn" LOCALIZED_STYLE_REF="default" FOLDED="true" ID="ID_512273540" CREATED="1649101691435" MODIFIED="1651432526205" COLOR="#000000" STYLE="fork">
 <font BOLD="false"/>
 <edge STYLE="bezier" COLOR="#808080"/>
+<node TEXT="# call: pip check" ID="ID_350357728" CREATED="1649593165660" MODIFIED="1649593175853"/>
+<node TEXT="# call: pip install .. and check sucess" ID="ID_1598755260" CREATED="1649593122392" MODIFIED="1649593164648"/>
+<node TEXT="# call: pip check" ID="ID_1466786505" CREATED="1649593140178" MODIFIED="1649593148662"/>
+<node TEXT="# update database" ID="ID_1705602847" CREATED="1649593149234" MODIFIED="1649593155355"/>
 </node>
 </node>
 <node TEXT="---" ID="ID_781208302" CREATED="1682368451520" MODIFIED="1682368453882"/>
@@ -161,16 +158,6 @@
 <node TEXT="[-linux|-osx|-windows]" LOCALIZED_STYLE_REF="default" ID="ID_764191577" CREATED="1648840754952" MODIFIED="1649102752775" COLOR="#000000" BACKGROUND_COLOR="#ffffff" STYLE="fork">
 <font BOLD="false"/>
 <edge STYLE="bezier" COLOR="#808080"/>
-</node>
-</node>
-<node TEXT="--packages_add" ID="ID_1017325688" CREATED="1651432469190" MODIFIED="1651432477570">
-<node TEXT="P0 P1 .. Pn" LOCALIZED_STYLE_REF="default" FOLDED="true" ID="ID_512273540" CREATED="1649101691435" MODIFIED="1651432526205" COLOR="#000000" STYLE="fork">
-<font BOLD="false"/>
-<edge STYLE="bezier" COLOR="#808080"/>
-<node TEXT="# call: pip check" ID="ID_350357728" CREATED="1649593165660" MODIFIED="1649593175853"/>
-<node TEXT="# call: pip install .. and check sucess" ID="ID_1598755260" CREATED="1649593122392" MODIFIED="1649593164648"/>
-<node TEXT="# call: pip check" ID="ID_1466786505" CREATED="1649593140178" MODIFIED="1649593148662"/>
-<node TEXT="# update database" ID="ID_1705602847" CREATED="1649593149234" MODIFIED="1649593155355"/>
 </node>
 </node>
 <node TEXT="--packages_delete" FOLDED="true" ID="ID_1590013358" CREATED="1651432478256" MODIFIED="1651432491565">
