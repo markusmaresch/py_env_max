a40d7e230713653bc43890b18ede0d98bee98674
Markus Maresch <markus.maresch@gmail.com>
2023-12-28 22:11:58 +0100

    added code and config for determining top level (orphan) packages

---
+++ cleanup_cmd.py
@@ -0,0 +1,53 @@
+#
+# -*- coding: utf-8 -*-
+#
+from database import Database
+
+
+class CleanupCmd:
+
+    @staticmethod
+    def top_level(env_name: str, force: bool = False) -> bool:
+        # Attempt to update existing python environment
+        print('top_level: {} (force={})'.format(env_name, force))
+        db_name = '{}.json'.format(env_name)
+        db = Database()
+        if not db.load(db_name):
+            # alternatively could call env_import and continue
+            return False
+
+        packages = db.packages_get_names_all()
+        num_packages = len(packages)
+
+        print('top_level: {}'.format(env_name))
+        print('Packages: total: {}'.format(num_packages))
+        num_top_level = 0
+        level = 0
+        while True:
+            level += 1
+            packages_per_level = db.packages_get_names_by_level(level=level)
+            if packages_per_level is None or len(packages_per_level) < 1:
+                break
+            # print('\t\tLevel\t{}:\t{}'.format(level, len(packages_per_level)))
+            num_level_top_level = 0
+            for package_name in packages_per_level:
+                required_by = db.package_get_required_by(package_name)
+                if required_by is None:
+                    continue
+                rb_len = len(required_by)
+                if rb_len > 0:
+                    # print(f'Level: {level} package_name: {package_name} required by: {required_by}')
+                    continue
+                summary = db.package_get_summary(package_name)
+                print(f'Top level or orphan: {level} / {package_name} .. {summary}')
+                num_top_level += 1
+                num_level_top_level += 1
+            # for
+            if num_level_top_level > 0:
+                print(f'Total for {level}: {num_level_top_level}')
+                print('\n')
+        # for
+
+        print('Top level or orphans: {}'.format(num_top_level))
+        db.close()
+        return True
