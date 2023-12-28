75c2b6d887f3a6acfae0bbc0670b40114c037db0
Markus Maresch <markus.maresch@gmail.com>
2023-12-28 22:12:28 +0100

    added missing py_env_nia scripts

---
+++ architecture/MacOSX-x86_64/py_env_nia_08.sh
@@ -0,0 +1,5 @@
+# 2023-12-28T19:47:51.903184+00:00
+# Level 8
+pip install "jupyter-server>=2.12.1"
+pip check
+# Level 8
---
+++ architecture/MacOSX-x86_64/py_env_nia_09.sh
@@ -0,0 +1,5 @@
+# 2023-12-28T19:47:51.903184+00:00
+# Level 9
+pip install "jupyter-lsp>=2.2.1"
+pip check
+# Level 9
