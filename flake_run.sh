391db6f42cc7288d5c272f189a9eed1de2526e19
Markus Maresch <markus.maresch@gmail.com>
2023-12-29 18:55:46 +0100

    improve linter environment

--- flake_run.sh
+++ flake_run.sh
@@ -1,16 +1,45 @@
-#!/bin/sh
+#!/bin/bash
 
 r=$(command -v flake8)
 if [ -z "$r" ]; then
     echo "flake8 ? .... pip install flake8"
 else
-    flake8 --ignore E127,E501,E722,W504 *.py
+    args="-v --ignore E127,E501,E722,W504,E203"
+    echo "FLAKE8 $args"
+    flake8 $args
+    ret=$?
+    #echo
+    if [ $ret -eq 0 ]; then
+      echo "FLAKE8 all good"
+    else
+      echo "FLAKE8: $ret .. ERROR"
+      exit $ret
+    fi
 fi
 
+echo
 r=$(command -v ruff)
 if [ -z "$r" ]; then
     echo "ruff ? .... pip install ruff"
 else
-    ruff --ignore E501,E722 .
+    echo "RUFF"
+    #ruff --ignore E501,E722 .
+    # E722 .. bare except
+    ruff --ignore E722 .
+    ret=$?
+    #echo
+    if [ $ret -eq 0 ]; then
+      echo "RUFF all good"
+    else
+      echo "RUFF: $ret .. ERROR"
+      exit $ret
+    fi
 fi
+exit 0
 
+#### example of .flake8 below
+
+[flake8]
+ignore = E302, E501, F401, F403
+max-line-length = 100
+max-complexity = 18
