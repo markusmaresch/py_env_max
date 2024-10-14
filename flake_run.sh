e9505e93ffaf4f9371b377f6f8f30193212c6476
Markus Maresch <markus.maresch@gmail.com>
2024-10-14 23:41:42 +0200

    reduce the allowed exception for flake8

--- flake_run.sh
+++ flake_run.sh
@@ -4,7 +4,7 @@ r=$(command -v flake8)
 if [ -z "$r" ]; then
     echo "flake8 ? .... pip install flake8"
 else
-    args="-v --ignore E127,E501,E722,W504,E203"
+    args="-v --ignore E501,E722,W504"
     echo "FLAKE8 $args"
     flake8 $args
     ret=$?
