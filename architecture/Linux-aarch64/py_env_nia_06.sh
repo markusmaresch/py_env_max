161591975c673a08855f11ba2518b8cc21f637b2
Markus Maresch <markus.maresch@gmail.com>
2025-03-26 17:50:14 +0100

    updated py_env_nia on aarch64

--- architecture/Linux-aarch64/py_env_nia_06.sh
+++ architecture/Linux-aarch64/py_env_nia_06.sh
@@ -1,4 +1,4 @@
-# 2024-12-06T13:08:00.865493+00:00
+# 2025-03-26T16:47:28.224339+00:00
 # Level 6
 pip install "google-cloud-bigquery>=3.27.0"
 pip install "google-cloud-storage>=2.19.0"
--- architecture/Linux-aarch64/py_env_nia_09.sh
+++ architecture/Linux-aarch64/py_env_nia_09.sh
@@ -1,4 +1,4 @@
-# 2024-12-06T13:08:00.865493+00:00
+# 2025-03-26T16:47:28.224339+00:00
 # Level 9
 pip install "jupyter-lsp>=2.2.5"
 pip install "jupyterlab-server>=2.27.3"
