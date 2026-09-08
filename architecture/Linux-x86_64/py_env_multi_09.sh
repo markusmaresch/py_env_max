eabd51b51caa3105bdd51477cec4b94d14a3492c
Markus Maresch <markus.maresch@gmail.com>
2026-09-09 00:42:51 +0200

    update versions for linux-x86_64

--- architecture/Linux-x86_64/py_env_multi_09.sh
+++ architecture/Linux-x86_64/py_env_multi_09.sh
@@ -1,4 +1,4 @@
-# 2026-07-27T21:15:22.560098+00:00
+# 2026-07-27T21:31:12.284824+00:00
 # Level 9
 pip install "jupyter-lsp>=2.3.1"
 pip install "jupyterlab-server>=2.28.0"
--- architecture/Linux-x86_64/py_env_multi_14.sh
+++ architecture/Linux-x86_64/py_env_multi_14.sh
@@ -1,4 +1,4 @@
-# 2026-07-27T21:15:22.560098+00:00
+# 2026-07-27T21:31:12.284824+00:00
 # Level 14
 pip install "jupyter-contrib-nbextensions>=0.7.0"
 pip check
